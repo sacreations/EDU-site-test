8f20c7ab948c2bf2a0c0f78db48b9eea19209050
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-03-16 13:52:33 +0530

    4.Hero.jsx and Hero.css components Create.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Hero from "./components/Hero/Hero.jsx";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
