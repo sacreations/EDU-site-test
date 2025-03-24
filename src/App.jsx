4a40d21fbaf6cb7b88e511b677bf543a8cf1d481
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-03-24 10:43:55 +0530

    13.Contact-Section-Created.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Title from "./components/Title/Title.jsx";
 import About from "./components/About/About.jsx";
 import Campus from "./components/Campus/Campus.jsx";
 import Testimonials from "./components/Testimonials/Testimonials.jsx";
+import Contact from "./components/contact/Contact.jsx";
 
 const App = () => {
   return (
@@ -20,6 +21,8 @@ const App = () => {
         <Campus />
         <Title subtitle="TESTIMONIALS" title="what student says" />
         <Testimonials />
+        <Title subtitle="Contact Us" title="Get in Touch" />
+        <Contact />
       </div>
     </div>
   );
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import "../contact/Contact.css";
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "f4fb6251-486e-4292-92ee-d024eec9ebab");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div className="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message <img src={msg_icon} alt="msg-icon" />
+        </h3>
+        <p>
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions, and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+        <ul>
+          <li>
+            <img src={mail_icon} alt="mail-icon" />
+            contact@GreatStack.dev
+          </li>
+          <li>
+            <img src={phone_icon} alt="phone-icon" />
+            +1 123-456-7890
+          </li>
+          <li>
+            <img src={location_icon} alt="location-icon" />
+            77 Massachusetts Ave, Cambridge MA 02139, United States
+          </li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your Name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter Your Mobile Number"
+            required
+          />
+          <label>Write Your Massages here</label>
+          <textarea
+            name="massage"
+            rows="6"
+            placeholder="Text your massage"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit Now <img src={white_arrow} alt="" />
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
