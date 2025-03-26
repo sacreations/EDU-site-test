1d8b913205b93492325ed24387dda463c85cd1cf
Supun Adithya <supunadithya@hotmail.com>
2025-03-26 11:47:20 +0530

    fix image import

--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Campus.css";
-import gallery_1 from "/Users/thiniranilushan/Documents/Git-Hub/EDU-Site/react-edu-app/src/assets/gallery-1.png";
+import gallery_1 from "../../assets/gallery-1.png";
 import gallery_2 from "../../assets/gallery-2.png";
 import gallery_3 from "../../assets/gallery-3.png";
 import gallery_4 from "../../assets/gallery-4.png";
